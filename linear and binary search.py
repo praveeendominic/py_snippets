c532c60dd1b7d006ba1cf189ba3fad0593c67c23
praveeendominic <61731613+praveeendominic@users.noreply.github.com>
2022-12-01 06:16:13 -0500

    linear and binary search

---
+++ linear and binary search.py
@@ -0,0 +1,35 @@
+
+def linear_search(num_list, search_key):
+    for index,elem in enumerate(num_list):
+        if elem == search_key:
+            return index
+    return -1
+
+def binary_search(num_list, search_key):
+    l = 0
+    r = len(num_list)-1
+    mid = 0
+    
+    while(l<=r):
+        mid = (l+r)//2
+        mid_elem = num_list[mid]
+        if search_key == mid_elem:
+            return mid
+        
+        if search_key > mid:
+            l = mid + 1
+        elif search_key < mid:
+            r = mid - 1
+        
+    
+    return -1
+
+
+
+if __name__ == '__main__':
+    num_list=[2,4,6,8,10,13,15,17,19,20]
+    # res = linear_search(num_list,search_key=19)
+    # print(res)
+    
+    res = binary_search(num_list,search_key=19)
+    print(res)
